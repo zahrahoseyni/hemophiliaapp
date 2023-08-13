8419efbae9c91c4eadb75e7912a8a8d6c12fe9d8
z.hoseini <z.hoseini@rad.holdings>
2023-08-13 21:08:23 +0330

    Add log out dialog

--- app/src/main/java/zahra/hosseini/hemophiliaapp/main/ui/root/ProfileScreen.kt
+++ app/src/main/java/zahra/hosseini/hemophiliaapp/main/ui/root/ProfileScreen.kt
@@ -1,31 +1,35 @@
 package zahra.hosseini.hemophiliaapp.main.ui.root
 
-import android.util.Log
-import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import zahra.hosseini.hemophiliaapp.R
 import zahra.hosseini.hemophiliaapp.authentication.AuthenticationViewModel
+import zahra.hosseini.hemophiliaapp.core.datastore.DataStoreManager
 import zahra.hosseini.hemophiliaapp.core.presentation.design_system.component.LargeDropdownMenu
 import zahra.hosseini.hemophiliaapp.core.presentation.design_system.component.RtlLabelInOutlineTextField
 import zahra.hosseini.hemophiliaapp.core.presentation.design_system.theme.hemophiliaColors
@@ -40,6 +44,8 @@ fun ProfileScreen(
     val scrollState = rememberScrollState()
     val context = LocalContext.current
     val coroutineScope = rememberCoroutineScope()
+    val logOutDialog = remember { mutableStateOf(false) }
+    val dataStoreManager = DataStoreManager(context)
 
     val (phoneNumber, setPhoneNumber) = remember { mutableStateOf("") }
     val (weight, setWeight) = remember { mutableStateOf("") }
@@ -47,13 +53,77 @@ fun ProfileScreen(
     val (age, setAge) = remember { mutableStateOf("") }
     val (timeOfDiagnosis, setTimeOfDiagnosis) = remember { mutableStateOf("") }
 
-    val sexOptions =
-        listOf(stringResource(id = R.string.woman), stringResource(id = R.string.man))
+    val sexOptions = listOf(stringResource(id = R.string.woman), stringResource(id = R.string.man))
     var sexSelectedIndex by remember { mutableStateOf(-1) }
 
     val familyHistoryOptions =
         listOf(stringResource(id = R.string.have), stringResource(id = R.string.have_not))
     var familyHistorySelectedIndex by remember { mutableStateOf(-1) }
+
+
+    if (logOutDialog.value) {
+        CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl)
+        {
+            AlertDialog(
+                onDismissRequest = {
+                    // Dismiss the dialog when the user clicks outside the dialog or on the back button.
+                    // If you want to disable that functionality, simply leave this block empty.
+                    logOutDialog.value = false
+                },
+                confirmButton = {
+                    TextButton(onClick = {
+                        // perform the confirm action and
+                        // close the dialog
+                        CoroutineScope(Dispatchers.IO).launch {
+                            dataStoreManager.storePhoneNumber(phoneNumber = phoneNumber)
+                            dataStoreManager.storeUserLogin(true)
+                        }
+                        navigateToLogin()
+                        logOutDialog.value = false
+                    }) {
+                        Text(
+                            text = stringResource(id = R.string.yes),
+                            style = MaterialTheme.hemophiliaTypography.text12,
+                            color = MaterialTheme.hemophiliaColors.designSystem.Neutral40
+                        )
+                    }
+                },
+                dismissButton = {
+                    TextButton(onClick = {
+                        // close the dialog
+                        logOutDialog.value = false
+                    }) {
+                        Text(
+                            text = stringResource(id = R.string.no),
+                            style = MaterialTheme.hemophiliaTypography.text12,
+                            color = MaterialTheme.hemophiliaColors.designSystem.Primary
+                        )
+                    }
+                },
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.logout_title),
+                        style = MaterialTheme.hemophiliaTypography.text14Medium,
+                        color = MaterialTheme.hemophiliaColors.designSystem.PrimaryText,
+                        // textAlign = TextAlign.End
+                    )
+                },
+                text = {
+                    Text(
+                        text = stringResource(id = R.string.logout_description),
+                        style = MaterialTheme.hemophiliaTypography.text12,
+                        color = MaterialTheme.hemophiliaColors.designSystem.Neutral50,
+                        // textAlign = TextAlign.End
+                    )
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(32.dp),
+                shape = RoundedCornerShape(5.dp),
+                // backgroundColor = Color.White
+            )
+        }
+    }
 
     LaunchedEffect(Unit) {
         coroutineScope.launch {
@@ -99,9 +169,7 @@ fun ProfileScreen(
                 .size(64.dp)
                 .clip(CircleShape)                       // clip to the circle shape
                 .border(
-                    2.dp,
-                    MaterialTheme.hemophiliaColors.designSystem.Neutral30,
-                    CircleShape
+                    2.dp, MaterialTheme.hemophiliaColors.designSystem.Neutral30, CircleShape
                 )
         )
 
@@ -171,8 +239,7 @@ fun ProfileScreen(
                 .background(color = MaterialTheme.hemophiliaColors.designSystem.Neutral00)
                 .padding(16.dp),
             border = BorderStroke(
-                width = 1.dp,
-                color = MaterialTheme.hemophiliaColors.designSystem.Neutral20
+                width = 1.dp, color = MaterialTheme.hemophiliaColors.designSystem.Neutral20
             ),
         ) {
             CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
@@ -186,10 +253,7 @@ fun ProfileScreen(
                     Text(
                         modifier = Modifier
                             .padding(
-                                start = 12.dp,
-                                end = 12.dp,
-                                top = 8.dp,
-                                bottom = 16.dp
+                                start = 12.dp, end = 12.dp, top = 8.dp, bottom = 16.dp
                             )
                             .clickable {
                                 navigateToAboutUs()
@@ -202,13 +266,10 @@ fun ProfileScreen(
                     Text(
                         modifier = Modifier
                             .padding(
-                                start = 12.dp,
-                                end = 12.dp,
-                                top = 8.dp,
-                                bottom = 16.dp
+                                start = 12.dp, end = 12.dp, top = 8.dp, bottom = 16.dp
                             )
                             .clickable {
-
+                                logOutDialog.value = true
                             },
                         text = stringResource(id = R.string.log_out),
                         style = MaterialTheme.hemophiliaTypography.text14Medium,
